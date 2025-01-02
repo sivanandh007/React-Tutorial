a826b36a955d53938ac70e8d07cdec86488d1b35
Sivanandh <sivanandhkatari007@gmail.com>
2025-01-02 17:09:28 +0530

    Styles in React

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Inline from './components/Inline';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
@@ -9,11 +10,20 @@ import './App.css';
 // import EventBind from './components/EventBind';
 // import ParentComponent from './components/ParentComponent';
 // import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+// import NameList from './components/NameList';
+// import Stylesheet from './components/Stylesheet';
+import './components/appStyles.css';
+import styles from './components/appStyles.module.css'
+
+
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className='error'>Error</h1>
+      <h1 className={styles.success}>Success</h1>
+      <Inline />
+      {/* <Stylesheet primary={true} /> */}
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
