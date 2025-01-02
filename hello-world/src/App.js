565d398d9c969e1fd336d799677e5185db01ddf1
Sivanandh <sivanandhkatari007@gmail.com>
2025-01-02 17:56:05 +0530

    Form Handling

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Inline from './components/Inline';
+// import Inline from './components/Inline';
 // import Greet from './components/Greet';
 // import Welcome from './components/Welcome';
 // import Hello from './components/Hello';
@@ -12,16 +12,17 @@ import Inline from './components/Inline';
 // import UserGreeting from './components/UserGreeting';
 // import NameList from './components/NameList';
 // import Stylesheet from './components/Stylesheet';
-import './components/appStyles.css';
-import styles from './components/appStyles.module.css'
-
+// import './components/appStyles.css';
+// import styles from './components/appStyles.module.css'
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <h1 className='error'>Error</h1>
+      <Form />
+      {/* <h1 className='error'>Error</h1>
       <h1 className={styles.success}>Success</h1>
-      <Inline />
+      <Inline /> */}
       {/* <Stylesheet primary={true} /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
